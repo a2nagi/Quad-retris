444f334099cd977b3081fdec7925edc1722a691d
Harkamal <harkamaljot9@gmail.com>
2017-11-25 12:26:20 -0500

    Added more grid stuff

--- model/Grid.h
+++ model/Grid.h
@@ -10,8 +10,12 @@ class Grid {
 private:
     std::vector<std::vector<Cell>> grid;
     Block* currentBlock;
+    int level;
+    int score;
 public:
     void insertNewBlock(Block *currentBlock);
     void updateCurrentBlock();
+    void getLevel();
+    void getScore();
 };
 #endif //GRID_H
