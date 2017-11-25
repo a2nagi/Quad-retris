df966b0da98fc43a224f4b451b93b307a7f8c415
Harkamal <harkamaljot9@gmail.com>
2017-11-25 10:58:18 -0500

    Added basic level class

--- controller/Level.h
+++ controller/Level.h
@@ -1,7 +1,14 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
-class Level {
+class Block;
 
+class Level {
+private:
+    int level;
+public:
+    virtual Block getNextBlock(char nextBlock) = 0;
+    virtual Block getNextBlock() = 0;
+    virtual Level getNextLevel() = 0;
 };
 #endif
