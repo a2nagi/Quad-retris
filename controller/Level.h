c188a1426747833d9002b7a325b4de61bbdac069
Harkamal <harkamaljot9@gmail.com>
2017-11-25 10:58:30 -0500

    Added level.h

--- controller/Level.h
+++ controller/Level.h
@@ -2,12 +2,14 @@
 #define LEVEL_H
 
 class Block;
-
+class OperationNotSupported {};
 class Level {
 private:
     int level;
 public:
-    virtual Block getNextBlock(char nextBlock) = 0;
+    virtual Block getNextBlock(char nextBlock) {
+        throw OperationNotSupported();
+    };
     virtual Block getNextBlock() = 0;
     virtual Level getNextLevel() = 0;
 };
