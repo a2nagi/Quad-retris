17be0891520fd3dc9b38ab02f4acd4f02ec1c4e8
Sarthak Raheja <sraheja@uwaterloo.ca>
2017-11-26 22:33:10 -0500

    Added controller

--- controller/Controller.h
+++ controller/Controller.h
@@ -1,6 +1,14 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <iostream>
+#include <regex>
+#include <ctype>
+#include <string>
+#include <fstream>
+#include "Block.h"
+#include "Level.h"
+
 class Controller {
 public:
     void parseCommand();
--- view/TextDisplay.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Harkamal on 2017-11-24.
-//
-
-#ifndef TEXTDISPLAY_H
-#define TEXTDISPLAY_H
-
-#endif //CS246_PROJECT_TEXTDISPLAY_H
