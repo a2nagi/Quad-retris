30b4a3d3fd5087ade3db84fbc66993b4f0be97af
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 17:26:57 -0500

    Update ZBlock.cpp
    Constructor for each kind of block , in Block Class constructor

--- ZBlock.cpp
+++ ZBlock.cpp
@@ -3,29 +3,29 @@ Color ZBlock::getColor() {
     return Color::PEACH;
 }
 
-ZBlock::ZBlock() {
+// ZBlock::ZBlock() {
 
-    Info z1;
-    z1.row=0;
-    z1.col=1;
-    z1.blockType='Z';
-    c1->setInfo(z1);
+//     Info z1;
+//     z1.row=0;
+//     z1.col=1;
+//     z1.blockType='Z';
+//     c1->setInfo(z1);
 
-    Info z2;
-    z2.row=0;
-    z2.col=2;
-    z2.blockType='Z';
-    c2->setInfo(z2);
+//     Info z2;
+//     z2.row=0;
+//     z2.col=2;
+//     z2.blockType='Z';
+//     c2->setInfo(z2);
 
-    Info z3;
-    z3.row=1;
-    z3.col=0;
-    z3.blockType='Z';
-    c3->setInfo(z3);
+//     Info z3;
+//     z3.row=1;
+//     z3.col=0;
+//     z3.blockType='Z';
+//     c3->setInfo(z3);
 
-    Info z4;
-    z4.row=1;
-    z4.col=1;
-    z4.blockType='Z';
-    c4->setInfo(z4);
-}
+//     Info z4;
+//     z4.row=1;
+//     z4.col=1;
+//     z4.blockType='Z';
+//     c4->setInfo(z4);
+// }
