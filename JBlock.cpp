1d8d556acd4a75f062c56f40b7a0dcc4a89d20a6
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 17:24:29 -0500

    Update JBlock.cpp
    Constructor for each kind of block , in Block Class constructor

--- JBlock.cpp
+++ JBlock.cpp
@@ -5,28 +5,28 @@ Color JBlock::getColor() {
 }
 
 
-JBlock::JBlock() {
-    Info j1;
-    j1.row=0;
-    j1.col=0;
-    j1.blockType='J';
-    c1->setInfo(j1);
+// JBlock::JBlock() {
+//     Info j1;
+//     j1.row=0;
+//     j1.col=0;
+//     j1.blockType='J';
+//     c1->setInfo(j1);
 
-    Info j2;
-    j2.row=0;
-    j2.col=1;
-    j2.blockType='J';
-    c2->setInfo(j2);
+//     Info j2;
+//     j2.row=0;
+//     j2.col=1;
+//     j2.blockType='J';
+//     c2->setInfo(j2);
 
-    Info i3;
-    i3.row=0;
-    i3.col=2;
-    i3.blockType='J';
-    c3->setInfo(i3);
+//     Info i3;
+//     i3.row=0;
+//     i3.col=2;
+//     i3.blockType='J';
+//     c3->setInfo(i3);
 
-    Info j4;
-    j4.row=1;
-    j4.col=0;
-    j4.blockType='J';
-    c4->setInfo(j4);
-}
+//     Info j4;
+//     j4.row=1;
+//     j4.col=0;
+//     j4.blockType='J';
+//     c4->setInfo(j4);
+// }
