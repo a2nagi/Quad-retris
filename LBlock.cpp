3450400c48fa8f24a69398750c3e763696d49df4
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 13:39:59 -0500

    Update LBlock.cpp
    Changes made to coordinates of figures as though being placed in 1st Quadrant XY plane.

--- LBlock.cpp
+++ LBlock.cpp
@@ -6,25 +6,25 @@ Color LBlock::getColor() {
 
 LBlock::LBlock() {
     Info l1;
-    l1.row=3;
+    l1.row=0;
     l1.col=0;
     l1.blockType='L';
     c1->setInfo(l1);
 
     Info l2;
-    l2.row=3;
+    l2.row=0;
     l2.col=1;
     l2.blockType='L';
     c2->setInfo(l2);
 
     Info l3;
-    l3.row=3;
+    l3.row=0;
     l3.col=2;
     l3.blockType='L';
     c3->setInfo(l3);
 
     Info l4;
-    l4.row=2;
+    l4.row=1;
     l4.col=2;
     l4.blockType='L';
     c4->setInfo(l4);
