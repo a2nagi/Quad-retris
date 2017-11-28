74dda7091b711f594d4bf40f92c23a5f0237e57d
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 13:38:46 -0500

    Update ZBlock.cpp
    Changes made to coordinates of figures as though being placed in 1st Quadrant XY plane.

--- ZBlock.cpp
+++ ZBlock.cpp
@@ -6,26 +6,26 @@ Color ZBlock::getColor() {
 ZBlock::ZBlock() {
 
     Info z1;
-    z1.row=2;
-    z1.col=0;
+    z1.row=0;
+    z1.col=1;
     z1.blockType='Z';
     c1->setInfo(z1);
 
     Info z2;
-    z2.row=2;
-    z2.col=1;
+    z2.row=0;
+    z2.col=2;
     z2.blockType='Z';
     c2->setInfo(z2);
 
     Info z3;
-    z3.row=3;
-    z3.col=1;
+    z3.row=1;
+    z3.col=0;
     z3.blockType='Z';
     c3->setInfo(z3);
 
     Info z4;
-    z4.row=3;
-    z4.col=2;
+    z4.row=1;
+    z4.col=1;
     z4.blockType='Z';
     c4->setInfo(z4);
 }
