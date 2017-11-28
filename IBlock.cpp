1824be4a9852a05a360cef3c752ff8e0119a8328
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 17:23:51 -0500

    Update IBlock.cpp
    Constructor for each kind of block , in Block Class constructor

--- IBlock.cpp
+++ IBlock.cpp
@@ -4,28 +4,28 @@ Color IBlock::getColor() {
    return Color::RED;
 }
 
-IBlock::IBlock() {
-    Info i1;
-    i1.row=0;
-    i1.col=0;
-    i1.blockType='I';
-    c1->setInfo(i1);
+// IBlock::IBlock() {
+//     Info i1;
+//     i1.row=0;
+//     i1.col=0;
+//     i1.blockType='I';
+//     c1->setInfo(i1);
 
-    Info i2;
-    i2.row=0;
-    i2.col=1;
-    i2.blockType='I';
-    c2->setInfo(i2);
+//     Info i2;
+//     i2.row=0;
+//     i2.col=1;
+//     i2.blockType='I';
+//     c2->setInfo(i2);
 
-    Info i3;
-    i3.row=0;
-    i3.col=2;
-    i3.blockType='I';
-    c3->setInfo(i3);
+//     Info i3;
+//     i3.row=0;
+//     i3.col=2;
+//     i3.blockType='I';
+//     c3->setInfo(i3);
 
-    Info i4;
-    i4.row=0;
-    i4.col=3;
-    i4.blockType='I';
-    c4->setInfo(i4);
-}
+//     Info i4;
+//     i4.row=0;
+//     i4.col=3;
+//     i4.blockType='I';
+//     c4->setInfo(i4);
+// }
