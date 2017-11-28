07a31f06013ab93557dc3f0f37e68afb5aa6a92c
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 13:38:01 -0500

    Update TBlock.cpp
    Changes made to coordinates of figures as though being placed in 1st Quadrant XY plane.

--- TBlock.cpp
+++ TBlock.cpp
@@ -6,25 +6,25 @@ Color TBlock::getColor() {
 TBlock::TBlock()
 {
     Info t1;
-    t1.row=3;
+    t1.row=0;
     t1.col=1;
     t1.blockType='T';
     c1->setInfo(t1);
 
     Info t2;
-    t2.row=2;
+    t2.row=1;
     t2.col=0;
     t2.blockType='T';
     c2->setInfo(t2);
 
     Info t3;
-    t3.row=2;
+    t3.row=1;
     t3.col=1;
     t3.blockType='T';
     c3->setInfo(t3);
 
     Info t4;
-    t4.row=2;
+    t4.row=1;
     t4.col=2;
     t4.blockType='T';
     c4->setInfo(t4);
