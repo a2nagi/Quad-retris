20a5d57622528ee665aeed574334d91c703e1a4f
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 13:35:54 -0500

    Update JBlock.cpp
    Changes made to coordinates of figures as though being placed in 1st Quadrant XY plane.

--- JBlock.cpp
+++ JBlock.cpp
@@ -7,26 +7,26 @@ Color JBlock::getColor() {
 
 JBlock::JBlock() {
     Info j1;
-    j1.row=2;
+    j1.row=0;
     j1.col=0;
     j1.blockType='J';
     c1->setInfo(j1);
 
     Info j2;
-    j2.row=3;
-    j2.col=0;
+    j2.row=0;
+    j2.col=1;
     j2.blockType='J';
     c2->setInfo(j2);
 
     Info i3;
-    i3.row=3;
-    i3.col=1;
+    i3.row=0;
+    i3.col=2;
     i3.blockType='J';
     c3->setInfo(i3);
 
     Info j4;
-    j4.row=3;
-    j4.col=2;
+    j4.row=1;
+    j4.col=0;
     j4.blockType='J';
     c4->setInfo(j4);
 }
