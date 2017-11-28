f5a2b5c20cc2a2e970a151d0e53667985ec3a43e
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 17:24:53 -0500

    Update LBlock.cpp
    Constructor for each kind of block , in Block Class constructor

--- LBlock.cpp
+++ LBlock.cpp
@@ -4,28 +4,28 @@ Color LBlock::getColor() {
     return Color::BLUE;
 }
 
-LBlock::LBlock() {
-    Info l1;
-    l1.row=0;
-    l1.col=0;
-    l1.blockType='L';
-    c1->setInfo(l1);
+// LBlock::LBlock() {
+//     Info l1;
+//     l1.row=0;
+//     l1.col=0;
+//     l1.blockType='L';
+//     c1->setInfo(l1);
 
-    Info l2;
-    l2.row=0;
-    l2.col=1;
-    l2.blockType='L';
-    c2->setInfo(l2);
+//     Info l2;
+//     l2.row=0;
+//     l2.col=1;
+//     l2.blockType='L';
+//     c2->setInfo(l2);
 
-    Info l3;
-    l3.row=0;
-    l3.col=2;
-    l3.blockType='L';
-    c3->setInfo(l3);
+//     Info l3;
+//     l3.row=0;
+//     l3.col=2;
+//     l3.blockType='L';
+//     c3->setInfo(l3);
 
-    Info l4;
-    l4.row=1;
-    l4.col=2;
-    l4.blockType='L';
-    c4->setInfo(l4);
-}
+//     Info l4;
+//     l4.row=1;
+//     l4.col=2;
+//     l4.blockType='L';
+//     c4->setInfo(l4);
+// }
