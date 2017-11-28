38d080ccc99fd452d1a321156b6f5e4d14f1dac5
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 17:26:13 -0500

    Update SBlock.cpp
    Constructor for each kind of block , in Block Class constructor

--- SBlock.cpp
+++ SBlock.cpp
@@ -3,29 +3,29 @@ Color SBlock::getColor() {
     return Color::VIOLET;
 }
 
-SBlock::SBlock()
-{
-    Info s1;
-    s1.row=0;
-    s1.col=0;
-    s1.blockType='S';
-    c1->setInfo(s1);
+// SBlock::SBlock()
+// {
+//     Info s1;
+//     s1.row=0;
+//     s1.col=0;
+//     s1.blockType='S';
+//     c1->setInfo(s1);
 
-    Info s2;
-    s2.row=0;
-    s2.col=1;
-    s2.blockType='S';
-    c2->setInfo(s2);
+//     Info s2;
+//     s2.row=0;
+//     s2.col=1;
+//     s2.blockType='S';
+//     c2->setInfo(s2);
 
-    Info s3;
-    s3.row=1;
-    s3.col=1;
-    s3.blockType='S';
-    c3->setInfo(s3);
+//     Info s3;
+//     s3.row=1;
+//     s3.col=1;
+//     s3.blockType='S';
+//     c3->setInfo(s3);
 
-    Info s4;
-    s4.row=1;
-    s4.col=2;
-    s4.blockType='S';
-    c4->setInfo(s4);
-}
+//     Info s4;
+//     s4.row=1;
+//     s4.col=2;
+//     s4.blockType='S';
+//     c4->setInfo(s4);
+// }
