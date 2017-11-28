ff246bd1d57c23b236ab36520a7b37b8c6d0fc1c
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-28 13:36:33 -0500

    Update OBlock.cpp
    Changes made to coordinates of figures as though being placed in 1st Quadrant XY plane.

--- OBlock.cpp
+++ OBlock.cpp
@@ -5,25 +5,25 @@ Color OBlock::getColor() {
 
 OBlock::OBlock() {
     Info o1;
-    o1.row=3;
+    o1.row=0;
     o1.col=0;
     o1.blockType='O';
     c1->setInfo(o1);
 
     Info o2;
-    o2.row=3;
+    o2.row=0;
     o2.col=1;
     o2.blockType='O';
     c2->setInfo(o2);
 
     Info o3;
-    o3.row=2;
+    o3.row=1;
     o3.col=0;
     o3.blockType='O';
     c3->setInfo(o3);
 
     Info o4;
-    o4.row=2;
+    o4.row=1;
     o4.col=1;
     o4.blockType='O';
     c4->setInfo(o4);
