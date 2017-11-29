27dabb29f4c4475f8c9b508cee38a869f765d203
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-29 12:46:09 -0500

    Update Block.h

--- controller/Block.h
+++ controller/Block.h
@@ -1,24 +1,33 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 #include <vector>
+#include "../model/Cell.h"
 #include "../model/State.h"
+
 
 class Cell;
 enum Color {RED, BLACK, PEACH, SKIN, TEAL, VIOLET, BLUE};
+
 class Block {
 protected:
+    int updatedHeight;
+    int updatedWidth;
     Cell *c1, *c2, *c3, *c4;
 public:
-    Block(char whichBlock);
+    Block(){};
     Block(const Block& other);
-    Block& operator=(const Block &b);
-    virtual void move(Direction d);
-    virtual void rotate(Rotate r);
+    Block& operator=(const Block &other);
+    void move(Direction d);
+    void rotate(Rotate r);
     std::vector<Cell *> getCells() {
         std::vector<Cell *> allCells = {c1,c2,c3,c4};
         return allCells;
     }
-    virtual Color getColor() = 0;
-    virtual ~Block();
+    virtual Color getColor()=0;
+    virtual void setRotationWidth(int width)=0;
+    virtual void setRotationHeight(int height)=0;
+    int getHeight();
+    int getWidth();
+    virtual ~Block(){};
 };
 #endif
