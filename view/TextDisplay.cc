73204a7d46742d3d6bb820fd8100a11371b42689
sarthakr <thesarthakraheja@gmail.com>
2017-11-28 21:11:13 -0500

    Add files via upload

---
+++ view/TextDisplay.cc
@@ -0,0 +1,50 @@
+#include "TextDisplay.h"
+
+using namespace std;
+
+TextDisplay::TextDisplay(Grid *g){
+	int rowLimit=18;
+	int colLimit=11;
+	for(int i=0;i<rowLimit;i++){
+		vector <char> row;
+		for(int j=0;j<colLimit;j++){
+			row.emplace_back(' ');
+		}
+		theDisplay.emplace_back(row);
+	}
+}
+
+TextDisplay::notify(subject &whoNotified){
+ 	Info I=whoNotified.getInfo();
+ 	theDisplay[I.r][I.c]=I.blockType;
+
+}
+
+
+ostream &operator<<(std::ostream &out, const TextDisplay &td){
+	out<<"Level:"<<"      "<<g.getLevel()<<endl;
+	out<<"Score:"<<"      "<<g.getScore()<<endl;
+	out<<"Hi Score:"<<"  "<<g.getHiScore()<<endl;
+	
+	int rowLimit=18;
+	int colLimit=11
+	for(int t=0;t<colLimit;t++){
+		out<<"-";
+	}
+	out<<endl;
+	for(int i=0;i<rowLimit;i++){
+		for(int j=0;j<colLimit;j++){
+			out<<theDisplay[i][j];
+		}
+	out<<endl;
+	}
+
+	for(int b=0;b<colLimit;b++){
+		out<<"-";
+	}
+	out<<endl;
+
+	out<<"Next:"<<endl;
+	//out<<NEXT BLOCK;
+	return out;	
+}
