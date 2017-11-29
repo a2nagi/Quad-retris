bf159b55cd43a388ad88f38c3ddcb6ff07375a81
sarthakr <thesarthakraheja@gmail.com>
2017-11-29 18:32:36 -0500

    Update Level2.h

--- controller/Level2.h
+++ controller/Level2.h
@@ -1,11 +1,22 @@
 #ifndef LEVEL2_H
 #define LEVEL2_H
 
+#include <cstdlib>
+#include "Level.h"
+#include "Block.h"
+#include "IBlock.h"
+#include "SBlock.h"
+#include "ZBlock.h"
+#include "OBlock.h"
+#include "JBlock.h"
+#include "TBlock.h"
+#include "LBlock.h"
+
 class Block;
 
-class Level {
+class Level2:public Level {
 
 public:
-	Block* getNextBlock(char nextBlock = '');
+	Block* getNextBlock() override;
 };
 #endif
