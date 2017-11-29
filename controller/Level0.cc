263a3775fa392bd08506e2f505619bf03e3b2e50
sarthakr <thesarthakraheja@gmail.com>
2017-11-29 18:30:23 -0500

    Added Level0.cc

---
+++ controller/Level0.cc
@@ -0,0 +1,57 @@
+#include "Level0.h"
+
+using namespace std;
+Level0::Level0(string filename): filename{filename}{
+	f= new ifstream(filename);
+	char c;
+	vector <char> seq;
+	while(*f>>c){
+		seq.emplace_back(c);
+	}
+
+}
+
+
+Block* Level0::getNextBlock(){
+	if(seq.size()==0){
+		return nullptr;
+	}
+	Block *b;
+
+    int currChar=seq.at(0);
+
+    if(currChar=='I'){
+		b= new IBlock();
+	}
+
+	else if(currChar=='S'){
+		b= new SBlock();
+	}
+
+	else if(currChar=='O'){
+		b= new OBlock();
+	}
+
+	else if(currChar=='T'){
+		b= new TBlock();
+	}
+
+	else if(currChar=='Z'){
+		b= new ZBlock();
+	}
+
+	else if(currChar=='J'){
+		b= new JBlock();
+	}
+	else if(currChar=='L'){
+		b= new LBlock();
+	}
+
+    seq.erase(seq.begin());
+
+	return b;
+}
+
+Level0::~Level0(){
+	delete f;
+}
