be438eb17e0c293cc0f9c692097ef32556d149a0
sarthakr <thesarthakraheja@gmail.com>
2017-11-29 15:19:22 -0500

    Level2 header files

--- controller/Level2.h
+++ controller/Level2.h
@@ -1,6 +1,16 @@
 #ifndef LEVEL2_H
 #define LEVEL2_H
 
+#include <cstdlib>
+#include "Block.h"
+#include "IBlock.h"
+#include "SBlock.h"
+#include "ZBlock.h"
+#include "OBlock.h"
+#include "JBlock.h"
+#include "TBlock.h"
+#include "OBlock.h"
+
 class Block;
 
 class Level {
