2f182bc829c96b894a335d2ffc9bcf88514568ac
sarthakr <thesarthakraheja@gmail.com>
2017-11-29 18:34:33 -0500

    Updated Level4.h

--- controller/Level4.h
+++ controller/Level4.h
@@ -1,10 +1,21 @@
 #ifndef LEVEL4_H
 #define LEVEL4_H
 
+#include <cstdlib>
+#include "Block.h"
+#include "IBlock.h"
+#include "SBlock.h"
+#include "ZBlock.h"
+#include "OBlock.h"
+#include "JBlock.h"
+#include "TBlock.h"
+#include "LBlock.h"
+#include "Level.h"
+
 class Block;
 
-class Level {
+class Level4:public Level{
 public:
-    Block *getNextBlock(char nextBlock = '') override;
+    Block *getNextBlock() override;
 };
 #endif
