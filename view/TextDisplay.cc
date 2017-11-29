b83ccb51834681b2b7a80d3ecbeb92ae22a1294f
sarthakr <thesarthakraheja@gmail.com>
2017-11-29 14:26:26 -0500

    Resolved Conflicts

--- view/TextDisplay.cc
+++ view/TextDisplay.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-<<<<<<< HEAD
+
 TextDisplay::TextDisplay(Grid *g){
 	int rowLimit=18;
 	int colLimit=11;
@@ -11,7 +11,7 @@ TextDisplay::TextDisplay(Grid *g){
 		for(int j=0;j<colLimit;j++){
 			row.emplace_back(' ');
 		}
-=======
+	}
 TextDisplay::TextDisplay(Grid *g)
 {
 	theGrid=g;
@@ -24,38 +24,24 @@ TextDisplay::TextDisplay(Grid *g)
 		{
 		    row.emplace_back(' ');
 		}
-		
->>>>>>> 38181108d106df1d8bd4e008a9bc23b601962b91
 		theDisplay.emplace_back(row);
 	}
 }
 
-<<<<<<< HEAD
-TextDisplay::notify(subject &whoNotified){
-=======
 TextDisplay::notify(subject &whoNotified)
 {
->>>>>>> 38181108d106df1d8bd4e008a9bc23b601962b91
  	Info I=whoNotified.getInfo();
  	theDisplay[I.r][I.c]=I.blockType;
-
 }
 
-
-<<<<<<< HEAD
-ostream &operator<<(std::ostream &out, const TextDisplay &td){
-	out<<"Level:"<<"      "<<g.getLevel()<<endl;
-=======
 ostream &operator<<(std::ostream &out, const TextDisplay &td)
 {
 	out<<"Level:"<<"      "<<g.getLevel()<<endl;//Not sure if these correlate to the actual funtions, but shouldnt be a task
->>>>>>> 38181108d106df1d8bd4e008a9bc23b601962b91
 	out<<"Score:"<<"      "<<g.getScore()<<endl;
 	out<<"Hi Score:"<<"  "<<g.getHiScore()<<endl;
 	
 	int rowLimit=18;
 	int colLimit=11
-<<<<<<< HEAD
 	for(int t=0;t<colLimit;t++){
 		out<<"-";
 	}
@@ -76,28 +62,3 @@ ostream &operator<<(std::ostream &out, const TextDisplay &td)
 	//out<<NEXT BLOCK;
 	return out;	
 }
-=======
-	for(int t=0;t<colLimit;t++)
-	{
-		out<<"-";
-	}
-	out<<endl;
-	for(int i=0;i<rowLimit;i++)
-	{
-	     for(int j=0;j<colLimit;j++)
-	     {
-		 out<<theDisplay[i][j];
-	     }
-	     out<<endl;
-	}
-
-	for(int b=0;b<colLimit;b++)
-	{
-		out<<"-";
-	}
-	out<<endl;
-        out<<"Next:"<<endl;
-	//out<<NEXT BLOCK;//Have to add Next Block Function
-	return out;	
-}
->>>>>>> 38181108d106df1d8bd4e008a9bc23b601962b91
