b258581c5949c9c14d8f90209696952083110b49
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-29 12:55:03 -0500

    setRotationHeight, setRotationWidth functions added

--- controller/IBlock.h
+++ controller/IBlock.h
@@ -3,10 +3,12 @@
 
 #include "Block.h"
 class IBlock : public Block {
+
 public:
     IBlock();
-    void move(Direction d);
-    void rotate(Rotate r);
-    Color getColor();
+    Color getColor()override;
+    void setRotationHeight(int height)override;
+    void setRotationWidth(int width)override;
+    ~IBlock();
 };
 #endif //IBLOCK_H
