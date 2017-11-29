c3b2797932544a70e053cdad574e0c36241d891a
sarthakr <thesarthakraheja@gmail.com>
2017-11-29 18:32:01 -0500

    fixed Level1.cc

--- controller/Level1.cc
+++ controller/Level1.cc
@@ -2,34 +2,36 @@
 
 using namespace std;
 
-Block* Level1::getNextBlock(char nextBlock = ''){
+Block* Level1::getNextBlock(){
 	int x = rand()%12;
 	
+	Block *b;
+
 	if(x==1 || x==4){
-		Block *b= new IBlock();
+		b= new IBlock();
 	}
 	else if(x==3 || x==5){
-		Block *b= new LBlock();
+		b= new LBlock();
 	}
 
 	else if(x==9 || x==7){
-		Block *b= new OBlock();
+		b= new OBlock();
 	}
 
 	else if(x==8 || x==11){
-		Block *b= new JBlock();
+		b= new JBlock();
 	}
 
 	else if(x==0 || x==2){
-		Block *b= new TBlock();
+		b= new TBlock();
 	}
 
 	else if(x==6){
-		Block *b= new SBlock();
+		b= new SBlock();
 	}
 
 	else if(x==10){
-		Block *b= new ZBlock();
+		b= new ZBlock();
 	}
 
 	return b;
