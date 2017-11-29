0e5352d5b718554631321d4c0188ba608dac3c84
sarthakr <thesarthakraheja@gmail.com>
2017-11-28 21:27:18 -0500

    Added Grid as a private member to the TextDisplay.h

--- view/TextDisplay.h
+++ view/TextDisplay.h
@@ -5,4 +5,22 @@
 #ifndef TEXTDISPLAY_H
 #define TEXTDISPLAY_H
 
+#include <iostream>
+#include <vector>
+#include "../Model/Observer.h"
+#include "../Model/Subject.h"
+#include "Grid.h"
+
+Class Cell;
+
+class TextDisplay:public Observer{
+	Grid theGrid;
+	std::vector<std::vector<char>> theDisplay;
+public:
+	TextDisplay();
+	void notify(subject &whoNotified) override;
+	friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
+};
+
+
 #endif //CS246_PROJECT_TEXTDISPLAY_H
