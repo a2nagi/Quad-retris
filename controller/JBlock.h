51c87c7a47582953a952290b27d0b5ac1ad43e96
Arjan Singh Nagi <a2nagi@uwaterloo.ca>
2017-11-29 12:55:55 -0500

    Update JBlock.h
    setRotationHeight/Width added

--- controller/JBlock.h
+++ controller/JBlock.h
@@ -4,8 +4,9 @@
 class JBlock : public Block {
 public:
     JBlock();
-    void move(Direction d);
-    void rotate(Rotate r);
-    Color getColor();
+    Color getColor()override;
+    void setRotationHeight(int height)override;
+    void setRotationWidth(int width)override;
+    ~JBlock();
 };
 #endif //JBLOCK_H
