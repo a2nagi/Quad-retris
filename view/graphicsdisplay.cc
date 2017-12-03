fe2528100f35f429a5073272a4936da035443a59
Harkamal Jot Kumar <hjskumar@uwaterloo.ca>
2017-12-03 18:14:30 -0500

    Fixed inverted grid

--- view/graphicsdisplay.cc
+++ view/graphicsdisplay.cc
@@ -2,15 +2,13 @@
 #include "graphicsdisplay.h"
 #include "../model/Info.h"
 #include "../model/Subject.h"
+#include "../model/Grid.h"
 
 using namespace std;
 
 GraphicsDisplay::GraphicsDisplay(int gridSize, int winSize, Grid *g): g{g}, xw{gridSize, winSize}  {
 
   xw.drawString(200, 20, "Project Quadris", Xwindow::Blue);
-  xw.drawString(100, 40, "Level:", Xwindow::Blue);
-  xw.drawString(100, 60, "Score:", Xwindow::Blue);
-  xw.drawString(100, 80, "Hi Score", Xwindow::Blue);
   xw.fillRectangle(0,150,500,500,Xwindow::Cyan);
 }
 
@@ -18,30 +16,33 @@ void GraphicsDisplay::notify(Subject &whoNotified) {
   Info I = whoNotified.getInfo();
   int cellColSize = 500 / 11;
   int cellRowSize = 500 / 18;
-
+  xw.drawString(100, 40, "Level: " + g->getLevel(), Xwindow::Blue);
+  xw.drawString(100, 60, "Score: " + g->getScore(), Xwindow::Blue);
+  xw.drawString(100, 80, "Hi Score " + g->getHighScore(), Xwindow::Blue);
+  int rows = 17;
   switch(I.blockType) {
    case 'I':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Green);
+      xw.fillRectangle(I.col*cellColSize,150+(rows - I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Green);
       break;
    case 'J':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Black);
+      xw.fillRectangle(I.col*cellColSize,150+(rows - I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Black);
       break;
    case 'L':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Blue);
+      xw.fillRectangle(I.col*cellColSize,150+(rows-I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Blue);
       break;
    case 'O':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Brown);
+      xw.fillRectangle(I.col*cellColSize,150+(rows-I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Brown);
       break;
    case 'S':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Yellow);
+      xw.fillRectangle(I.col*cellColSize,150+(rows-I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Yellow);
       break;
    case 'Z':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Magenta);
+      xw.fillRectangle(I.col*cellColSize,150+(rows - I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Magenta);
       break;
    case 'T':
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Orange);
+      xw.fillRectangle(I.col*cellColSize,150+(rows - I.row)*cellRowSize,cellColSize,cellRowSize, Xwindow::Orange);
        break;
      default:
-      xw.fillRectangle(I.col*cellColSize,150+I.row*cellRowSize,cellColSize,cellRowSize, Xwindow::Cyan);
+      xw.fillRectangle(I.col*cellColSize,150+(rows - I.row) *cellRowSize,cellColSize,cellRowSize, Xwindow::Cyan);
   }
 }
