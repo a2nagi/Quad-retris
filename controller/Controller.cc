b46bf33a4f75681026a39e8c8e1bf46aff23acf5
Harkamal Jot Kumar <hjskumar@uwaterloo.ca>
2017-12-04 13:43:05 -0500

    Fixed another regex match bug

--- controller/Controller.cc
+++ controller/Controller.cc
@@ -20,12 +20,12 @@ Controller::Controller(Grid *g,TextDisplay *td): g{g},td{td} {
     drop = new regex("dr");
     levelup = new regex("levelu");
     leveldown = new regex("leveld");
-    norandom = new regex("nor");
-    random = new regex("ra");
-    sequence = new regex("s");
+    norandom = new regex("^nor");
+    random = new regex("^ra");
+    sequence = new regex("^s");
     block = new regex("^[IJLOSZT]$");
- 	restart = new regex("re");
- 	hint = new regex("h");
+ 	restart = new regex("^re");
+ 	hint = new regex("^h");
 
     cout << *td;
 };
@@ -42,7 +42,39 @@ void Controller::parseCommand(istream *inputStream){
             multiple = 1;
         }
 
-        if(regex_search(s,*left)){
+        if(regex_search(s,*norandom)){
+            istringstream noRandomFileName{s};
+            string fileName;
+            noRandomFileName >> fileName >> fileName;
+            g->makeCurrentLevelFromFile(fileName);
+        }
+
+        else if(regex_search(s, *random)) {
+            g->makeCurrentLevelRandom();
+        }
+
+        else if(regex_search(s, *sequence)) {
+            istringstream sequenceFile{s};
+            string fileName;
+            sequenceFile >> fileName >> fileName;
+            fstream *file = new fstream(fileName);
+            parseCommand(file);
+            delete file;
+        }
+        else if(regex_search(s, *block)) {
+            g->replaceCurrentBlock(s.at(0));
+            cout << *td << endl;
+        }
+        else if(regex_search(s, *restart)) {
+            g->clearGrid();
+            cout << *td << endl;
+        }
+        else if(regex_search(s, *hint)) {
+            g->showHint();
+            cout << *td << endl;
+        }
+
+        else if(regex_search(s,*left)){
             try {
                 g->moveCurrentBlockLeftRight(Direction::left, multiple);
                 cout<<*td;
@@ -111,37 +143,6 @@ void Controller::parseCommand(istream *inputStream){
             g->levelDown(multiple);
         }
 
-        else if(regex_search(s,*norandom)){
-            istringstream noRandomFileName{s};
-            string fileName;
-            noRandomFileName >> fileName >> fileName;
-            g->makeCurrentLevelFromFile(fileName);
-        }
-
-        else if(regex_search(s, *random)) {
-            g->makeCurrentLevelRandom();
-        }
-
-        else if(regex_search(s, *sequence)) {
-            istringstream sequenceFile{s};
-            string fileName;
-            sequenceFile >> fileName >> fileName;
-            fstream *file = new fstream(fileName);
-            parseCommand(file);
-            delete file;
-        }
-        else if(regex_search(s, *block)) {
-            g->replaceCurrentBlock(s.at(0));
-            cout << *td << endl;
-        }
-        else if(regex_search(s, *restart)) {
-            g->clearGrid();
-            cout << *td << endl;
-        }
-        else if(regex_search(s, *hint)) {
-            g->showHint();
-            cout << *td << endl;
-        }
         else {
             cout << "Invalid command! Please try again" << endl;
         }
